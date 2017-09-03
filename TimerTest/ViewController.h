008a26e60a034b876a13cd233eacedaaff07af40
nick <164049933@qq.com>
2017-09-03 18:12:20 +0800

    Initial Commit

---
+++ TimerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerTest
+//
+//  Created by baidu on 2017/9/3.
+//  Copyright © 2017年 gamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TimerTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimerTest
+//
+//  Created by baidu on 2017/9/3.
+//  Copyright © 2017年 gamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
